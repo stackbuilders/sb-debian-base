6a117076e45c5fa1e755d93c91eab7bbdb3fae23
Sebastián Estrella <sestrella.me@gmail.com>
2018-10-18 11:26:22 -0500

    Setup molecule test admin authorized_keys

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_admin_authorized_keys(host):
+    f = host.file('/home/admin/.ssh/authorized_keys')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == 'admin'
+    assert f.group == 'admin'
+    assert f.contains('ADMIN_USER_1')
+    assert f.contains('ADMIN_USER_2')
