671b15f55c8b3fff7b45fb61bcb5c49d05268a84
speedlight <ceguez@stackbuilders.com>
2019-04-26 11:34:48 -0500

    WIP adding molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,6 +13,15 @@ def test_admin_user(host):
     assert u.groups == ['admin', 'sudo']
 
 
+def test_sudoers_owner(host):
+    f = host.file('etc/sudoers')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.contains('NOPASSWD: ALL')
+
+
 def test_admin_authorized_keys_file(host):
     f = host.file('/home/admin/.ssh/authorized_keys')
 
@@ -21,3 +30,33 @@ def test_admin_authorized_keys_file(host):
     assert f.group == 'admin'
     assert f.contains('ADMIN_USER_1')
     assert f.contains('ADMIN_USER_2')
+
+
+def test_sshd_is_installed(host):
+    sshd = host.package("openssh-server")
+    assert sshd.is_installed
+
+
+def test_sshd_config(host):
+    f = host.file('/etc/ssh/sshd_config')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.contains('PasswordAuthentication no')
+    assert f.contains('PermitRootLogin no')
+
+
+def test_deploy_user(host):
+    u = host.user('deploy')
+    assert u.shell == '/bin/bash'
+    assert u.home == '/home/deploy'
+
+
+def test_deploy_authorized_keys_file(host):
+    f = host.file('/home/deploy/.ssh/authorized_keys')
+
+    assert f.exists
+    assert f.user == 'deploy'
+    assert f.group == 'deploy'
+    assert f.contains('DEPLOY_USER_1')
